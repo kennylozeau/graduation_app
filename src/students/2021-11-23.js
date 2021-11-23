b4c80967efac8b74e7705bf92a837ce5d57e7f52
Kenny Lozeau <kenny.lozeau@gmail.com>
2021-11-23 18:27:19 -0500

    Update students for next graduation

---
+++ src/students/2021-11-23.js
@@ -0,0 +1,220 @@
+const students = [
+    {
+        "id": 6816,
+        "name": "Adesh Pawaroo",
+        "imageUrl": "https://secure.gravatar.com/avatar/9cf205437926994153a37af87c1288d8?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6794,
+        "name": "Albert Kim",
+        "imageUrl": "https://secure.gravatar.com/avatar/4d79881f551aa8f682c81638e7c8fb72?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6785,
+        "name": "Alberto Young",
+        "imageUrl": "https://secure.gravatar.com/avatar/41d36af3c603abe471a5234b5e0867bc?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6802,
+        "name": "Daniel Bradley",
+        "imageUrl": "https://secure.gravatar.com/avatar/8830451b202077307b6fac3811087606?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6783,
+        "name": "Efren Bahena",
+        "imageUrl": "https://secure.gravatar.com/avatar/9a9f7d7bccb02d82efe3eb580384967c?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6775,
+        "name": "Elliot Wilson",
+        "imageUrl": "https://secure.gravatar.com/avatar/935b4cdc6e7abebecaf0668104bc5d49?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6779,
+        "name": "Emily Wu",
+        "imageUrl": "https://secure.gravatar.com/avatar/b96aac5df21cf8fa8dc3ed4e6e47cad9?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6774,
+        "name": "Farzam Ahmad",
+        "imageUrl": "https://secure.gravatar.com/avatar/fa03f7be29540e5c153a6ddc0ab664e9?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6579,
+        "name": "Feifei Yang",
+        "imageUrl": "https://secure.gravatar.com/avatar/09e4a5f03ee58181c2ac1613379705e7?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6793,
+        "name": "George Tsimis",
+        "imageUrl": "https://secure.gravatar.com/avatar/c9f7167cee2b8460ad080812c1f703a7?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6623,
+        "name": "Hien Nguyen",
+        "imageUrl": "https://secure.gravatar.com/avatar/a91347c10a547c70c0b9cf62d28e8361?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6781,
+        "name": "Jonathan Berzon",
+        "imageUrl": "https://secure.gravatar.com/avatar/6e07e759378e76e86222ec7e9b71433d?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6784,
+        "name": "Jonathan Ortiz",
+        "imageUrl": "https://secure.gravatar.com/avatar/63546c4413287498e9e2dc1eea492cd7?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6806,
+        "name": "Madeline Wilson",
+        "imageUrl": "https://secure.gravatar.com/avatar/e35d57bbb1cba3c602b2245ad2d0caad?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6673,
+        "name": "Maisie Bruno-Tyne",
+        "imageUrl": "https://secure.gravatar.com/avatar/6d93cdbc69a1fdaf471be7d8cd0590f0?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6778,
+        "name": "Mansoo Kim",
+        "imageUrl": "https://secure.gravatar.com/avatar/ff3fe83ad8a0a4c190b8dbf5364ea389?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6776,
+        "name": "Maria Vaghani",
+        "imageUrl": "https://secure.gravatar.com/avatar/ae94ee1c44309e29ae661a9b33fed55b?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6814,
+        "name": "Matteo Rossant",
+        "imageUrl": "https://secure.gravatar.com/avatar/47c2d6b7578557feeca0c268b9188cf5?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6803,
+        "name": "Matthew (Matt) Swedin",
+        "imageUrl": "https://secure.gravatar.com/avatar/18e3e44761968093ceeb8fd6960d6926?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6799,
+        "name": "Michelle (Mickey) Addai",
+        "imageUrl": "https://secure.gravatar.com/avatar/4b4af7bfd4aac944772c901f0c2e59a3?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6798,
+        "name": "Mohammed (Emmay) Alam",
+        "imageUrl": "https://secure.gravatar.com/avatar/ebcd5f8a9dd1512db70e25082a765b12?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6818,
+        "name": "Neil Pandya",
+        "imageUrl": "https://secure.gravatar.com/avatar/c9237d079002b5feb615d6c07fc7b76d?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6782,
+        "name": "Pasan Dharmasena",
+        "imageUrl": "https://secure.gravatar.com/avatar/9d3e79e6f11ae987c5af51ba2efe8996?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6626,
+        "name": "Pierre Navarin",
+        "imageUrl": "https://secure.gravatar.com/avatar/cd2b0db841fb9465e27aff4f138fef2e?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6804,
+        "name": "Rebecca Foster",
+        "imageUrl": "https://secure.gravatar.com/avatar/88addcaf6fd886ad087df436b1476734?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6712,
+        "name": "Snigdha Banda",
+        "imageUrl": "https://secure.gravatar.com/avatar/d6128f151060cb920c3c8b71068d34d3?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6801,
+        "name": "Sohee Park",
+        "imageUrl": "https://secure.gravatar.com/avatar/683f2e331692322aa9e876a05eab5192?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6770,
+        "name": "Stella Kang",
+        "imageUrl": "https://secure.gravatar.com/avatar/1b9cde9b5440aa30a1ebbd4c4e44b504?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6795,
+        "name": "Subin Cho",
+        "imageUrl": "https://secure.gravatar.com/avatar/c29d5885f46b0b26eaead8861b20d7b5?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6772,
+        "name": "Suzanne Li",
+        "imageUrl": "https://secure.gravatar.com/avatar/6b06e3e645f8b4a66c36b7e077df0a1b?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6585,
+        "name": "Sydney Parsons",
+        "imageUrl": "https://secure.gravatar.com/avatar/f9fe38748f7988acd5f0f57214d74458?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6817,
+        "name": "Tyler Koh",
+        "imageUrl": "https://secure.gravatar.com/avatar/f902735459364e971085ea94b12fff6f?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6790,
+        "name": "Victor He",
+        "imageUrl": "https://secure.gravatar.com/avatar/560d8118515be4ba18805d8c0ee3a6ea?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6555,
+        "name": "Yehuda Goldschein",
+        "imageUrl": "https://secure.gravatar.com/avatar/628f7c0eacc42bc967ec2fe9156dda10?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6813,
+        "name": "Zayed Cheema",
+        "imageUrl": "https://secure.gravatar.com/avatar/74a29d92307c23ae224b8fc96bcdc99d?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 6805,
+        "name": "Zeki Kural",
+        "imageUrl": "https://secure.gravatar.com/avatar/0cb68454e72e7f285a035b3a14ee6d80?secure=true&size=300",
+        "occup": "student"
+    }
+];
+
+export default students;
