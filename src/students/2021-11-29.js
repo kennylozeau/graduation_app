49feeeab3fcfcb50ea046950516b5fdc35aada0f
Kenny Lozeau <kenny.lozeau@gmail.com>
2022-03-25 09:13:06 -0400

    Update for 2021-11-29 cohort

---
+++ src/students/2021-11-29.js
@@ -0,0 +1,118 @@
+const students = [
+    {
+        "id": 7203,
+        "name": "Aagam Vakil",
+        "imageUrl": "https://secure.gravatar.com/avatar/f7e2daa115110fd03a5bd366fa2a1ce9?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7217,
+        "name": "Alex Clark",
+        "imageUrl": "https://media-exp1.licdn.com/dms/image/C5603AQGNJ2nyCvkHzQ/profile-displayphoto-shrink_800_800/0/1517243806919?e=1653523200&v=beta&t=rLB3pweiUGV5W8VkBgesKtOL4-VdbnKpW0Bi69SfUB4",
+        "occup": "student"
+    },
+    {
+        "id": 7207,
+        "name": "Bill Camarco",
+        "imageUrl": "https://secure.gravatar.com/avatar/a075e128b3561725577fcf48f585fd05?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7219,
+        "name": "Ding Tian (Dean) Ding",
+        "imageUrl": "https://secure.gravatar.com/avatar/186a1f477eef8d358aaf12fe0061c39c?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7204,
+        "name": "Emily Lichtenberg",
+        "imageUrl": "https://secure.gravatar.com/avatar/9d1feecacf4d882d67c38bc6c51ee88e?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7202,
+        "name": "Ethan Gumin",
+        "imageUrl": "https://secure.gravatar.com/avatar/524df88586cfd9ceb4ae548c37ba1519?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7214,
+        "name": "Ethan Lam",
+        "imageUrl": "https://secure.gravatar.com/avatar/3ff2391214ea3985fd13b3637ab26f9d?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7146,
+        "name": "Ian Farr",
+        "imageUrl": "https://secure.gravatar.com/avatar/76d9d9ca4e8a2d0ffb6138d1f0f257dd?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7215,
+        "name": "Jonathan Kim",
+        "imageUrl": "https://secure.gravatar.com/avatar/4321533f1b7383ea059e9a8d06bcafa3?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7057,
+        "name": "Ka (Ka-Yi) Leung",
+        "imageUrl": "https://secure.gravatar.com/avatar/de5ffacdf7b5198a22c37e5dabc591c0?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7221,
+        "name": "Kirby Neaton",
+        "imageUrl": "https://secure.gravatar.com/avatar/f2b78ee340a126b2d3899bb85ee9ef41?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7216,
+        "name": "Qien Gao",
+        "imageUrl": "https://secure.gravatar.com/avatar/ca0a3457718773ee4e61bb87222f9ad2?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7205,
+        "name": "Sean Mackey",
+        "imageUrl": "https://secure.gravatar.com/avatar/2cd069e3f6cbd9e1c747685d1befb347?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7223,
+        "name": "Shawn Fries",
+        "imageUrl": "https://secure.gravatar.com/avatar/7f8f13841bdb6871a0cc4bc577def4fa?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7211,
+        "name": "Steven Wu",
+        "imageUrl": "https://secure.gravatar.com/avatar/49d77a2fc42dc4357f2e2ed5d0d52f7e?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7049,
+        "name": "Yaakov Zaetz",
+        "imageUrl": "https://media-exp1.licdn.com/dms/image/C4E03AQGmJ3l_wvQDMg/profile-displayphoto-shrink_800_800/0/1517594813971?e=1653523200&v=beta&t=W-kzMByPy2UV9Ir-l3FpX8yUvy3i7YrLjkHAOLOVM54",
+        "occup": "student"
+    },
+    {
+        "id": 7222,
+        "name": "Yao Z. (Yao) Xu",
+        "imageUrl": "https://student-images-klo.s3.amazonaws.com/yao_img.jpg",
+        "occup": "student"
+    },
+    {
+        "id": 7208,
+        "name": "Yong Woo (Bill) Kim",
+        "imageUrl": "https://secure.gravatar.com/avatar/f1f80e15c64885ebe873488022fca3fe?secure=true&size=300",
+        "occup": "student"
+    },
+    {
+        "id": 7210,
+        "name": "Yuchen (Katie) Hang",
+        "imageUrl": "https://secure.gravatar.com/avatar/fe997d45eb2558fba5257dd49aa06c35?secure=true&size=300",
+        "occup": "student"
+    }
+]
+
+export default students;
